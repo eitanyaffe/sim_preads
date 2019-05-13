c876df5953811a2d67bc9972cde8b3f656ba3d43
Eitan Yaffe <eitan.yaffe@gmail.com>
2019-05-13 12:29:26 -0700

    .

--- generate.sh
+++ generate.sh
@@ -5,13 +5,13 @@ SET=$1
 mkdir -p out/$SET
 
 # generate assembly (fasta)
-./R_call.r R/gen_assembly.r gen.$SET \
+Rscript R_call.r R/gen_assembly.r gen.$SET \
 	   ofn.fasta="out/$SET/mol.fasta" \
 	   ofn.table="out/$SET/mol.tab"
 rc=$?; if [ $rc -ne 0 ]; then exit 1; fi
 
 # generate read coordinates
-./R_call.r R/gen_read_coords.r gen.read.coords \
+Rscript R_call.r R/gen_read_coords.r gen.read.coords \
 	   genome.table.ifn="out/$SET/mol.tab" \
 	   insert=200 insert.sd=100 read.length=150 \
 	   ofn="out/$SET/mol.coords"
@@ -22,7 +22,7 @@ perl pl/generate_reads_pair.pl out/$SET/mol.coords out/$SET/mol.fasta out/$SET/R
 rc=$?; if [ $rc -ne 0 ]; then exit 1; fi
 
 # plotting
-./R_call.r R/plot_read_distrib.r plot.read.distrib \
+Rscript R_call.r R/plot_read_distrib.r plot.read.distrib \
 	   ifn="out/$SET/mol.coords" \
 	   fdir="out/$SET/plots"
 rc=$?; if [ $rc -ne 0 ]; then exit 1; fi
